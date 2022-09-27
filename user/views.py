31f52376c5e3c847b2f57ce3e12d0db5ea653003
rangersui <2506488479@qq.com>
2022-09-27 19:14:11 +0800

    添加user的view登录功能的重定向和提示功能

--- user/views.py
+++ user/views.py
@@ -3,16 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render, redirect
-
-
+from . import models
 def index(request):
     pass
     return render(request, 'user/index.html')
 
 
 def login(request):
-    pass
-    return render(request, 'user/login.html')
+    if request.method == "POST":  # 通过post请求读取到了用户名和密码
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username == '' and password == '':
+            message = "请输入用户名和密码！"
+            return render(request, 'user/login.html', {"message": message})
+        if username and password:
+            username = username.strip()
+            try:
+                current_user = models.User.objects.get(name=username)
+                if current_user.password == password:
+                    return redirect('/index')  # 正确就重定向到主页，并处于登陆状态
+                else:
+                    message = "密码不正确！"
+            except:
+                message = "用户不存在！"
+            return render(request, 'user/login.html', {"message": message})
+    return render(request, 'user/login.html')  # 直接按则没用
 
 
 def register(request):
