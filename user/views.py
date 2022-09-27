89e610f4606b59fe2d5080b95ac1e21d50c71ba7
rangersui <2506488479@qq.com>
2022-09-27 20:27:25 +0800

    user的form表单前端后端编写
    将从POST的请求html中直接从input读取改为使用django的form表单，并解决无法从GET中前端显示表单的问题

--- user/views.py
+++ user/views.py
@@ -4,20 +4,26 @@ from django.shortcuts import render
 
 from django.shortcuts import render, redirect
 from . import models
+from . import forms
+
+
 def index(request):
     pass
     return render(request, 'user/index.html')
 
 
 def login(request):
-    if request.method == "POST":  # 通过post请求读取到了用户名和密码
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        if username == '' and password == '':
-            message = "请输入用户名和密码！"
-            return render(request, 'user/login.html', {"message": message})
-        if username and password:
-            username = username.strip()
+    if request.method == "GET":
+        login_form = forms.UserForm()
+        return render(request, 'user/login.html', {"login_form": login_form})
+    elif request.method == "POST":  # 通过post请求读取到了用户名和密码
+        login_form = forms.UserForm(request.POST)
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+        message = "请检查填写的内容！"
+        if login_form.is_valid():  # 换成表单可以避免不明意义的判断
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
             try:
                 current_user = models.User.objects.get(name=username)
                 if current_user.password == password:
@@ -26,8 +32,8 @@ def login(request):
                     message = "密码不正确！"
             except:
                 message = "用户不存在！"
-            return render(request, 'user/login.html', {"message": message})
-    return render(request, 'user/login.html')  # 直接按则没用
+            return render(request, 'user/login.html', {"message": message,"login_form": login_form})
+        return render(request, 'user/login.html', {"message": message, "login_form": login_form})
 
 
 def register(request):
